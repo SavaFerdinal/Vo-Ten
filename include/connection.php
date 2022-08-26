669f65d5aafbe454055d853ec44f1f02d471323c
SavaFerdinal <sava.ferdinal@gmail.com>
2022-08-26 15:00:15 +0700

    26/08/22

--- include/connection.php
+++ include/connection.php
@@ -4,8 +4,14 @@ defined('BASEPATH') or die("No access direct allowed");
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$db   = 'vote';
+$db   = 'voten';
 
-$con  = new mysqli($host, $user, $pass, $db) or die(mysqli_error());
+// Create connection
+$con = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+if ($con->connect_error) {
+  die("Connection failed: " . $con->connect_error);
+}
 
 ?>
